f4786354d74d96a12d61c869493da260e559b901
duduluu <duduluu@outlook.com>
2019-04-29 19:21:38 +0800

    extract: free area building

---
+++ Jsonify/Asset/FreeAreaBuilding.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Anno1800.Jsonify {
+  partial class Asset {
+
+    class FreeAreaProductivity : BaseAssetObject {
+      public int radius;
+      public int neededAreaPercent;
+      public int worker;
+      public int maxWorkers;
+      public int workerPause;
+      public int wayTime;
+      public string? freeAreaType;
+      public bool cutTree;
+
+      public FreeAreaProductivity(XElement element) : base(element) {
+        this.radius = element.Int("InfluenceRadius");
+        this.neededAreaPercent = element.Int("NeededAreaPercent");
+        this.worker = element.Int("WorkerUnit");
+        this.maxWorkers = element.Int("MaxWorkerAmount");
+        this.workerPause = element.Int("WorkerPause");
+        this.wayTime = element.Int("WayTime");
+        this.freeAreaType = element.String("FreeAreaType");
+        this.cutTree = element.Boolean("CutTree");
+      }
+    }
+
+    [Adapter]
+    class FreeAreaBuilding : FactoryBuilding7 {
+      public FreeAreaProductivity? freeAreaProductivity;
+
+      public FreeAreaBuilding(XElement asset, Dictionary<string, XElement> map) : base(asset, map) {
+        var values = asset.Element("Values");
+        this.freeAreaProductivity = values.Object<FreeAreaProductivity>("FreeAreaProductivity");
+      }
+    }
+  }
+}
---
+++ Jsonify/Asset/HeavyFreeAreaBuilding.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Anno1800.Jsonify {
+  partial class Asset {
+
+    [Adapter]
+    class HeavyFreeAreaBuilding : FreeAreaBuilding {
+
+      public HeavyFreeAreaBuilding(XElement asset, Dictionary<string, XElement> map) : base(asset, map) {
+        var values = asset.Element("Values");
+      }
+    }
+  }
+}
